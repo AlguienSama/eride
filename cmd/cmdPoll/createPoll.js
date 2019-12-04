5b8345b6034a6a7109d1ae7043d15d02887c02de
Glitch (fresh-nigella) <none>
2019-12-04 12:49:39 +0000

    🐈🏵 Checkpoint ./cmd/cmdPoll/poll:5115704/531 ./cmd/cmdPoll/createPoll.js:5115704/1600

--- cmd/cmdPoll/createPoll.js
+++ cmd/cmdPoll/createPoll.js
@@ -39,23 +39,22 @@ module.exports = {
                         })
 
                         let pollEmbed2 = new Discord.RichEmbed()
-                            await poll.establecer(`${message.guild.id}.${pollCommand}.${pollName}`)
                             .setTitle('Poll name: '+ pollName)
                             .setDescription('Poll command: '+ pollCommand)
                             .setTimestamp()
                 
                         oneCard.on('collect', async () => {
                             pollEmbed2.addField('Poll type', 'Una única carta disponible')
-                            pollEmbed2.addField('Información!', 'Solo puede haber una sola colección con el mismo nombre'
-                                +'pero pueden haber distintas colecciones con el mismo comando')
-                            return message.channel.send(pollEmbed2)
+                            await poll.establecer(`${message.guild.id}.${pollCommand}.${pollName}.type`, "one").then(() => {
+                                return message.channel.send(pollEmbed2)
+                            }).catch(err => error(m ))
                         })
                 
                         multCard.on('collect', async () => {
                             pollEmbed2.addField('Poll type', 'Una carta por usuario')
-                            pollEmbed2.addField('Información!', 'Solo puede haber una sola colección con el mismo nombre'
-                                +'pero pueden haber distintas colecciones con el mismo comando')
-                            return message.channel.send(pollEmbed2)
+                            await poll.establecer(`${message.guild.id}.${pollCommand}.${pollName}.type`, "one").then(() => {
+                                return message.channel.send(pollEmbed2)
+                            }).catch(err => error())
                         })
     
                         await mensaje.react("650668882994135051").catch(err => error(mensaje, "Reaction createPoll 01 => ", err))
