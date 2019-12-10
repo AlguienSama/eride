91345f99f7c95e057feea77908510afe1236a116
Glitch (fresh-nigella) <none>
2019-12-10 09:04:36 +0000

    🐾🎍 Checkpoint ./cmd/cmdGame/snow.js:5115704/848

--- cmd/cmdGame/snow.js
+++ cmd/cmdGame/snow.js
@@ -1,4 +1,4 @@
-onst Discord = require('discord.js')
+const Discord = require('discord.js')
 const client = new Discord.Client()
 const db = require('megadb')
 let poll = new db.crearDB('polls')
@@ -13,9 +13,25 @@ module.exports = {
   
     run: async (message, args, prefix) => {
         if (message.mentions.users.first())
-          `${message.mentions.users.first()} te están retando a una pelea de nieve!`)
+          var msg = `${message.mentions.users.first()} te están retando a una pelea de nieve!`
         else 
-          message.channel.send(message.author +' quiere hacer una pelea de nieve. Quien acepta? ``'+prefix+'acept``')
+          var msg = message.author +' quiere hacer una pelea de nieve. Quien acepta? ``'+prefix+'acept``'
+          
+        message.channel.send(msg)
+      
+        if (message.mentions.users.first())
+          var collector = new Discord.MessageCollector(message.channel, msg => msg.author.id === message.mentions.users.first().id, { time: 30000 });
+        else
+          var collector = new Discord.MessageCollector(message.channel, { time: 30000 });
+      
+      const player1 = message.author
+      
+      collector.on('collect', async msg => {
+          const player2 = msg.author
+          
+          message.channel.send("Player1 "+ player1 +"\nPlayer2 "+ player2)
+      })
+        
         
     }
 }
