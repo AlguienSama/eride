e63b6e77039a70504475ff1d3855f2f74a4296cc
Glitch (fresh-nigella) <none>
2019-12-12 19:07:32 +0000

    🛥🐌 Checkpoint ./cmd/cmdPoll/snow.js:5115704/1819

--- cmd/cmdPoll/snow.js
+++ cmd/cmdPoll/snow.js
@@ -1,39 +1,18 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const db = require('megadb')
-let poll = new db.crearDB('polls')
+let game = new db.crearDB('games')
 
 var { error, deny } = require('../../logs.js')
 
 module.exports = {
-    name:'pelear',
-    alias:['newcard'],
-    description:'Añadir una nueva carta personalizada',
-    usage:'``addcard``',
-  
     run: async (message, args, prefix) => {
         if (message.mentions.users.first())
-          var msg = `${message.mentions.users.first()} te están retando a una pelea de nieve!`
+            var msg = `${message.mentions.users.first()} te están retando a una pelea de nieve!`
         else 
-          var msg = message.author +' quiere hacer una pelea de nieve. Quien acepta? ``'+prefix+'acept``'
-          
+            var msg = message.author +' quiere hacer una pelea de nieve. Quien acepta? ``'+prefix+'acept``'
+        
+            game.establecer(`${message.channel.id}.player1.id`, message.author.id)
         message.channel.send(msg)
-
-        const filter = m => {
-          m.content.includes(prefix+'acept');
-          if (message.mentions.users.first())
-            message.mentions.users.first().id === m.author.id
-        }
-        const collector = message.channel.createMessageCollector(filter, { time: 15000 });
-
-      const player1 = message.author
-      
-      collector.on('collect', async m => {
-          const player2 = m.author
-          
-          message.channel.send("Player1 "+ player1 +"\nPlayer2 "+ player2)
-      })
-        
-        
     }
 }
