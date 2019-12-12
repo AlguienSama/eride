022cf265cacf4e515cc67bfc42618164370f1be5
Glitch (fresh-nigella) <none>
2019-12-12 19:53:13 +0000

    🐀👍 Checkpoint ./snow.js:5115704/920

--- snow.js
+++ snow.js
@@ -12,16 +12,29 @@ module.exports = {
         const command = args.shift().toLowerCase()
 
         if (command == "start") {
-            if (game.tiene(`${message.guild.id}`)) return message.channel.send("Ya hay una pelea en juego")
-            if (message.mentions.users.first())
+            if (game.tiene(`${message.channel.id}`)) return message.channel.send("Ya hay una pelea en juego")
+            if (message.mentions.users.first()) {
                 var msg = `${message.mentions.users.first()} te están retando a una pelea de nieve!`
                 game.establecer(`${message.channel.id}.retado`, message.mentions.users.first().id)
+            }
             else 
                 var msg = message.author +' quiere hacer una pelea de nieve. Quien acepta? ``'+prefix+'acept``'
             
             game.establecer(`${message.channel.id}.player1.id`, message.author.id)
             game.establecer(`${message.channel.id}.player1.life`, 3)
             message.channel.send(msg)
+        }
+
+        if (command == "accept") {
+            if (!game.tiene(`${message.channel.id}`)) 
+                return message.channel.send("Debes de iniciar una pelea antes")
+            if (await game.obtener(`${message.channel.id}.retado`) != message.author.id) 
+                return message.channel.send("Tu no eres el usuario retado")
+
+            game.establecer(`${message.channel.id}.player2.id`, message.author.id)
+            game.establecer(`${message.channel.id}.player2.life`, 3)
+
+            
         }
         
     }
