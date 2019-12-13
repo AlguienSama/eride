8feb884f77087ce64ad675b52b40cdffed55898c
Glitch (fresh-nigella) <none>
2019-12-13 18:45:13 +0000

    ❄️🎪 Checkpoint ./cosasNazis.js:5115704/1657 ./index.js:5115704/23

--- cosasNazis.js
+++ cosasNazis.js
@@ -1,24 +1,20 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
 const db = require('megadb')
 let game = new db.crearDB('games')
 
 var { error, deny } = require('./logs.js')
 
 module.exports = {
-    cosasNazis: async clientTest => {
-        clientTest.on("message", message => {
-            const filter = m => m.author.id == a || m.author.id == a;
+    cosasNazis: async (client, message, prefix) => {
 
-            client.users.get("355104003572498435").send("Hola")
-            .then(() => {
-                message.channel.awaitMessages(filter, { time: 3000 })
-                .then(collected => {
-                    console.log(collected)
-                })
+        const filter = m => m.author.id == a || m.author.id == a;
+
+        client.users.get("355104003572498435").send("Hola")
+        .then(() => {
+            message.channel.awaitMessages(filter, { time: 3000 })
+            .then(collected => {
+                console.log(collected)
             })
         })
-        
-        
     }
 }
