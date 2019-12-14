3776ee384baee41f71ceb2bdc2459486d14cf933
Glitch (fresh-nigella) <none>
2019-12-14 19:59:00 +0000

    🌋🕋 Checkpoint ./snow.js:5115704/8311

--- snow.js
+++ snow.js
@@ -104,7 +104,29 @@ async function startGame(message) {
     const filter = m => m.author.id == player1.id || m.author.id == player2.id;
 
     var ronda = setInterval(() => {
-        
+        if (player1.getVida() == 0 || player2.getVida() == 0) {
+            let finEmbed = new Discord.RichEmbed()
+                .setTitle("Pelea de bolas de nieve")
+                .setColor("#d0d0ff")
+                .addField(player1.name, `Anterior acción: ${player1.getAccion()}\n♥️ Vida: ${player1.getVida()}`, true)
+                .addField(player2.name, `Anterior acción: ${player2.getAccion()}\n♥️ Vida: ${player2.getVida()}`, true)
+            if (player1.getVida() == 0 && player2.getVida() == 0) {
+                finEmbed.setDescription(`❄️***EMPATE***❄️`)
+                message.channel.send(finEmbed)
+                clearInterval(ronda)
+                return
+            } else if (player1.getVida() == 0) {
+                finEmbed.setDescription(`❄️***VICTIORA de ${player2.name}***❄️`)
+             message.channel.send(finEmbed)
+                clearInterval(ronda)
+                return
+            } else if (player2.getVida() == 0) {
+                finEmbed.setDescription(`❄️***VICTIORA de ${player1.name}***❄️`)
+                message.channel.send(finEmbed)
+                clearInterval(ronda)
+                return
+            }
+        }
         var time = 3;
         let fightEmbed = new Discord.RichEmbed()
             .setTitle("Pelea de bolas de nieve")
@@ -152,32 +174,7 @@ async function startGame(message) {
                                     return
                                 }
                                 console.log("Vida 1 == "+ player1.getVida() + "\nVida 2 == " + player2.getVida())
-                                let finEmbed = new Discord.RichEmbed()
-                                    .setTitle("Pelea de bolas de nieve")
-                                    .setColor("#d0d0ff")
-                                    .addField(player1.name, `Anterior acción: ${player1.getAccion()}\n♥️ Vida: ${player1.getVida()}`, true)
-                                    .addField(player2.name, `Anterior acción: ${player2.getAccion()}\n♥️ Vida: ${player2.getVida()}`, true)
-                                if (player1.getVida() == 0 && player2.getVida() == 0) {
-                                    finEmbed.setDescription(`❄️***EMPATE***❄️`)
-                                    message.channel.send(finEmbed)
-                                    clearInterval(ataque)
-                                    clearInterval(ronda)
-                                    return
-                                } else if (player1.getVida() == 0) {
-                                    finEmbed.setDescription(`❄️***VICTIORA de ${player2.name}***❄️`)
-                                    message.channel.send(finEmbed)
-                                    clearInterval(ataque)
-                                    clearInterval(ataque)
-                                    clearInterval(ronda)
-                                    return
-                                } else if (player2.getVida() == 0) {
-                                    finEmbed.setDescription(`❄️***VICTIORA de ${player1.name}***❄️`)
-                                    message.channel.send(finEmbed)
-                                    clearInterval(ataque)
-                                    clearInterval(ataque)
-                                    clearInterval(ronda)
-                                    return
-                                }
+                                
                             })
                         })
                         
@@ -240,4 +237,3 @@ function doAction(player1, player2) {
                 player1.damage(1)
     }
 }
-// clearInterval()
