272ac53f8b9c73da4d60e6869f86c6c051df0707
Glitch (fresh-nigella) <none>
2019-12-15 12:39:56 +0000

    🐘👫 Checkpoint ./cosasNazis.js:5115704/351

--- cosasNazis.js
+++ cosasNazis.js
@@ -5,16 +5,7 @@ let game = new db.crearDB('games')
 var { error, deny } = require('./logs.js')
 
 module.exports = {
-    // cosasNazis: async (client, message, prefix) => {
+    cosasNazis: async (client, message) => {
 
-    //     const filter = m => m.author.id == a || m.author.id == a;
-
-    //     client.users.get("355104003572498435").send("Hola")
-    //     .then(() => {
-    //         message.channel.awaitMessages(filter, { time: 3000 })
-    //         .then(collected => {
-    //             console.log(collected)
-    //         })
-    //     })
-    // }
+    }
 }
