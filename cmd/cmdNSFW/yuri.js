b8276b69ffd8780c5a55794cd067608fbcf3e224
Glitch (fresh-nigella) <none>
2020-01-03 01:40:58 +0000

    🚞🍩 Checkpoint ./cmd/cmdNSFW/blowJob.js:5115704/856 ./cmd/cmdNSFW/cum.js:5115704/912 ./cmd/cmdNSFW/classic.js:5115704/930 ./cmd/cmdNSFW/yuri.js:5115704/878

---
+++ cmd/cmdNSFW/yuri.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = require('nekos.life');
+const neko = new client();
+const db = require('megadb')
+let bbdd = new db.crearDB('permisos')
+
+const { imgEmbed, imgSpoiler } = require('../../files/embeds.js');
+
+module.exports = {
+    name: 'yuri',
+    alias: [],
+    description: 'Hentai yuri',
+    usage: '``yuri``',
+
+    run: async (message, args) => {
+        
+        neko.nsfw.yuri().then(async img => {
+
+            if (bbdd.tiene(`${message.guild.id}.nsfw`)) {
+                let perm = await bbdd.obtener(`${message.guild.id}.nsfw`)
+    
+                if (perm === "spoiler")
+                    return message.channel.send( await imgSpoiler(img.url))
+            } else {
+                return message.channel.send( await imgEmbed(img.url))
+            }
+        })
+    }
+};
