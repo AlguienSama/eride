a753e610ed339007d40a71ffcb448ae986454ae8
Glitch (fresh-nigella) <none>
2020-01-06 23:12:53 +0000

    🍚🌎 Checkpoint ./cmd/cmdFun/gif.js:5115704/771

---
+++ cmd/cmdFun/gif.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+var giphy = require('giphy-api')();
+
+const { imgEmbed } = require('../../files/embeds.js');
+
+module.exports = {
+    name: 'gif',
+    alias: [],
+    description: 'Un gif random',
+    usage: 'gif <argumentos>',
+    permission:'none',
+    type:'fun',
+
+    run: async (message, args) => {
+        
+        if (!args) {
+            giphy.trending({
+                limit: 1,
+                rating: 'g',
+                fmt: 'json'
+            }).then(async img => {
+                console.log(img.data.image_url);
+                return message.channel.send(await imgEmbed(img.data.image_url))
+                
+            })
+        }
+
+        giphy.random(args.join(" ")).then(async img => {
+            console.log(img.data.image_url);
+            return message.channel.send(await imgEmbed(img.data.image_url))
+            
+        })
+
+        // neko.sfw.lick().then(async img => {
+        //     let desc = `${message.author} ha lamido a ${args.join(" ")}`;
+        //     return message.channel.send(await imgDescEmbed(desc, img.url))
+        // })
+    }
+};
