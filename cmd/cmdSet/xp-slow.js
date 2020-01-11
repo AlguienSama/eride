e38e960450392a84d61aee15ee3d54f338e8c0bd
Glitch (fresh-nigella) <none>
2020-01-11 20:26:41 +0000

    🌊🐝 Checkpoint ./cmd/cmdSet/xp-slow.js:5115704/300 ./cmd/cmdSet/xp.js:5115704/4768

--- cmd/cmdSet/xp-slow.js
+++ cmd/cmdSet/xp-slow.js
@@ -12,7 +12,7 @@ module.exports = {
     '**Slow:** Se gana xp por 1 mensaje con una cuenta atrás de los segundos especificados (0 por defecto)',
     usage:'xp-type < segundos >',
     permission:'Administrador',
-    type:'set',
+    type:'xp',
   
     run: async (message, args) => {
         
@@ -21,6 +21,10 @@ module.exports = {
         if (!args[0])
             return message.channel.send("``xp-slow < segundos >``\nTiempo actual: **" + await xp.obtener(`${message.guild.id}.slow`) + '** segundos')
 
-        let sec = parseInt()
+        let sec = parseInt(args[0])
+
+        await xp.establecer(`${message.guild.id}.slow`, sec).then(() => {
+            return message.channel.send('Slow cambiado a **' + sec + '** segundos')
+        })
     }
 }
---
+++ cmd/cmdSet/xp.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const db = require('megadb')
+let xp = new db.crearDB('xp')
+
+const { error } = require('../../files/logs.js');
+const { admin } = require('../../files/perm.js');
+
+module.exports = {
+    name:'xp',
+    alias:['level'],
+    description:'Ver la experiencia ganada\n',
+    usage:'xp [ usuario ]',
+    permission:'nonce',
+    type:'xp',
+  
+    run: async (message, args) => {
+        
+        let userXp = await xp.obtener(`${message.guild.id}.users.${message.author.id}.xp`).catch(err => error(message, "Obtener xp usuario 001", err));
+        return message.channel.send(userXp)
+
+    }
+}
