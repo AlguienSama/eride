ce70106ad7ad5f4d3e088cdcea2ab2d7c8230c76
Glitch (fresh-nigella) <none>
2020-01-11 18:50:43 +0000

    💒🎊 Checkpoint ./xp.js:5115704/2434

--- xp.js
+++ xp.js
@@ -11,17 +11,37 @@ module.exports = {
 
         let listChannels = await xp.obtener(`${message.guild.id}.channelsDisable`);
 
-        if (listChannels.tiene(message.channel.id)) return;
+        if (listChannels.tiene(message.channel.id)) 
+            return;
+
+        if (!xp.tiene(`${message.guild.id}.users.${message.author.id}`))
+            await xp.establecer(`${message.guild.id}.users.${message.author.id}`, { xp: 0, lastMessage: 0 });
 
         if (await xp.obtener(`${message.guild.id}.config`) === "default") {
 
-            if (!xp.tiene(`${message.guild.id}.users.${message.author.id}`))
-                await xp.establecer(`${message.guild.id}.users.${message.author.id}`, { xp: 0, lastMessage: 0 });
+            sumarXP(message);
 
-            let xpVal = console.log(Math.floor(Math.random()*10)+10);
-            
-            xp.sumar(`${message.guild.id}.users.${message.author.id}.xp`, xpVal)
+        } 
+        
+        else if (await xp.obtener(`${message.guild.id}.config`) === "slow") {
 
+            if (await xp.obtener(`${message.guild.id}.users.${message.author.id}.lastMessage` > message.createdAt.getTime())) 
+                return;
+
+            sumarXP(message);
+            setTimer(message);
         }
     }
+}
+
+function sumarXP(message) {
+    let xpVal = console.log(Math.floor(Math.random()*10)+10);
+            
+    xp.sumar(`${message.guild.id}.users.${message.author.id}.xp`, xpVal)
+}
+
+function setTimer(message) {
+    let time = message.createdAt.getTime()+60000;
+
+    await xp.establecer(`${message.guild.id}.users.${message.author.id}.lastMessage`, time);
 }
