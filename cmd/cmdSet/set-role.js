8271e453a427b07c56eb2618ae64f1e5914805e3
Glitch (fresh-nigella) <none>
2020-01-13 17:04:12 +0000

    🚛🏹 Checkpoint ./cmd/cmdSet/set-role.js:5115704/1919

---
+++ cmd/cmdSet/set-role.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const db = require('megadb')
+
+const { error } = require('../../files/logs.js');
+const { admin } = require('../../files/perm.js');
+
+module.exports = {
+    name:'add-role',
+    alias:['añadir-rol'],
+    description:'Agregar un rol a un usuario (everyone para todos)',
+    usage:'add-role <usuario> <nombre del rol>',
+    permission:'Administrador',
+    type:'set',
+  
+    run: async (message, args) => {
+        
+        admin(message)
+        
+        if (!args[0] || !args[1])
+            return message.channel.send("Comando mal escrito\n``add-role <usuario> <nombre del rol>``")
+
+        let user = args.shift();
+        let role = message.guild.roles.find(n => n.name === args.join(" "))
+
+        if (user == "everyone") {
+            let users = [message.guild.members.map(m => m.user)];
+            let totalUsers = 
+            users[0].forEach(u => {
+                
+            });
+        }
+    }
+}
