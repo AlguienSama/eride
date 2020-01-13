269eed07307878d3897c5477a1135af081ad2593
Glitch (fresh-nigella) <none>
2020-01-13 17:16:12 +0000

    🐊👷 Checkpoint ./cmd/cmdSet/set-role.js:5115704/1309

--- cmd/cmdSet/set-role.js
+++ cmd/cmdSet/set-role.js
@@ -20,13 +20,25 @@ module.exports = {
             return message.channel.send("Comando mal escrito\n``add-role <usuario> <nombre del rol>``")
 
         let user = args.shift();
-        let role = message.guild.roles.find(n => n.name === args.join(" "))
+        let role = message.guild.roles.find(n => n.name === args.join(" ")) || message.guild.roles.get(args[1]);
+
+        if (role == undefined)
+            return message.channel.send("Rol no encontrado!")
 
         if (user == "everyone") {
             let users = [message.guild.members.map(m => m.user)];
-            let totalUsers = 
+            let totalUsers = 0;
             users[0].forEach(u => {
-                
+                let member = message.guild.members.get(u.id)
+                console.log(member)
+                if (!member.roles.has(role.id)) {
+                    member.addRole(role.id);
+                    totalUsers++;
+                }
+            }).then(() => {
+                return message.channel.send(`${role.name} agregado correctamente a ${totalUsers} usuarios!`)
+            }).catch((err) => {
+                console.log(err)
             });
         }
     }
